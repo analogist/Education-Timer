dafebf42c0daceb18449da680ffb827cdb2da7b6
James Wu <james@analogist.net>
2017-10-13 11:11:39 -0700

    working Light-only mode

--- EduTimer/EduTimer.ino
+++ EduTimer/EduTimer.ino
@@ -7,13 +7,14 @@
 #include <Wire.h>
 
 #define SERIAL_DEBUG 0
-#define DEBOUNCE_TIME 80
+#define DEBOUNCE_TIME 150
 
-#define BUTTON_GO 2
-#define BUTTON_MODE 4
-#define BUTTON_RESET 5
+#define BUTTON_GO 0
+#define BUTTON_RESET 1
+#define BUTTON_MODE 2
 
 #define BUZZER 3
+#define GOLIGHT 5
 #define BACKLIGHT 6
 
 #define MODE_LIGHT 0
@@ -48,45 +49,57 @@ void setup()
   randomSeed(analogRead(A5));
 
 	pinMode(BACKLIGHT, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
+  pinMode(GOLIGHT, OUTPUT);
+  pinMode(BUTTON_GO_LIGHT, OUTPUT);
 	analogWrite(BACKLIGHT, 200);
 }
 
 void loop()
 {
-  if(! digitalRead(BUTTON_GO))
-  {
   delay(DEBOUNCE_TIME);
   
-  runtask(taskmode);
+  if(! digitalRead(BUTTON_GO))
+    runtask(taskmode);
+  if(! digitalRead(BUTTON_MODE)) {
+    if(taskmode >= 2)
+      taskmode = 0;
+    else
+      taskmode++;
   }
 }
 
-unsigned long runtask(int run_mode)
+void runtask(int run_mode)
 {
   lcd.setCursor(0, 2);
   delay(500);
   lcd.print("Get ready for ");
+  
   switch(taskmode) {
     case MODE_LIGHT:
       lcd.print("light!");
       break;
   }
   
-  go_waittime = random(1500, 5000);
+  go_waittime = random(1500, 6500);
   delay(go_waittime);
-  analogWrite(BACKLIGHT, 100); // GO!
+
+  switch(taskmode) {
+    case MODE_LIGHT:
+      analogWrite(BACKLIGHT, 150); // GO!
+      break;
+  }
+  
   go_signaltime = millis();
   while(digitalRead(BUTTON_GO));
   go_respondtime = millis() - go_signaltime;
   analogWrite(BACKLIGHT, 200);
-  lcd.setCursor(0, 3);
-  lcd.print(go_respondtime);
-  lcd.print(" millisec");
-
-  return go_respondtime;
+  dispmessage(run_mode, go_respondtime);
 }
 
-void dispmessage(int run_mode)
+void dispmessage(int run_mode, unsigned long go_respondtime)
 {
-	
+  lcd.setCursor(0, 3);
+  lcd.print(String (float (go_respondtime)/1000, 3));
+  lcd.print(" sec");
 }
