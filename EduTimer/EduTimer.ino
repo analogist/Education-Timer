8c7f080f1360b1e7c16f5c3c026196be95dc7e13
James Wu <james@analogist.net>
2017-10-13 01:42:07 -0700

    add minimal response time function

--- EduTimer/EduTimer.ino
+++ EduTimer/EduTimer.ino
@@ -6,17 +6,27 @@
 #include <LiquidCrystal.h>
 #include <Wire.h>
 
-#define DEBUGGING 0
+#define SERIAL_DEBUG 0
 #define DEBOUNCE_TIME 80
 
-#define BUTTON_RESPONSE 2
+#define BUTTON_GO 2
 #define BUTTON_MODE 4
 #define BUTTON_RESET 5
 
 #define BUZZER 3
 #define BACKLIGHT 6
 
-bool stimmode 0;
+#define MODE_LIGHT 0
+#define MODE_BUZZ 1
+#define MODE_INTERFERE 2
+
+// Runtime variables
+int taskmode = MODE_LIGHT;
+bool taskstart = false;
+bool gosignal = false;
+unsigned long go_waittime = 0;
+unsigned long go_signaltime = 0;
+unsigned long go_respondtime = 0;
 
 LiquidCrystal lcd(7, 8, 9, 10, 11, 12);
 
@@ -27,27 +37,56 @@ void setup()
 	lcd.setCursor(0, 1);
 	lcd.print("Mode: Light");
 
-
-	#if DEBUGGING
+	#if SERIAL_DEBUG
 		Serial.begin(9600);
 		while(!Serial);
 	#endif
 
 	pinMode(BUTTON_MODE, INPUT_PULLUP);
-	pinMode(BUTTON_RESPONSE, INPUT_PULLUP);
+	pinMode(BUTTON_GO, INPUT_PULLUP);
 	pinMode(BUTTON_RESET, INPUT_PULLUP);
+  randomSeed(analogRead(A5));
 
 	pinMode(BACKLIGHT, OUTPUT);
-
-	analogWrite(BACKLIGHT, 70);
+	analogWrite(BACKLIGHT, 200);
 }
 
 void loop()
 {
-	
+  if(! digitalRead(BUTTON_GO))
+  {
+  delay(DEBOUNCE_TIME);
+  
+  runtask(taskmode);
+  }
 }
 
-void dispmessage(stimmode)
+unsigned long runtask(int run_mode)
+{
+  lcd.setCursor(0, 2);
+  delay(500);
+  lcd.print("Get ready for ");
+  switch(taskmode) {
+    case MODE_LIGHT:
+      lcd.print("light!");
+      break;
+  }
+  
+  go_waittime = random(1500, 5000);
+  delay(go_waittime);
+  analogWrite(BACKLIGHT, 100); // GO!
+  go_signaltime = millis();
+  while(digitalRead(BUTTON_GO));
+  go_respondtime = millis() - go_signaltime;
+  analogWrite(BACKLIGHT, 200);
+  lcd.setCursor(0, 3);
+  lcd.print(go_respondtime);
+  lcd.print(" millisec");
+
+  return go_respondtime;
+}
+
+void dispmessage(int run_mode)
 {
 	
 }
